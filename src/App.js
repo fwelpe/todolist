8f04ec8ee0fea54f0226e7914614a30b5e3b9d78
fwelpe <wheel0@mail.ru>
2019-10-08 22:15:47 +0300

    sync

--- src/App.js
+++ src/App.js
@@ -5,16 +5,53 @@ import Footer from './components/Footer.js';
 import todosData from './todosData'
 import TodoItem from './TodoItem'
 
+
 class App extends React.Component {
   constructor() {
     super()
     this.state = {
-      todos: todosData
+      todos: todosData,
+      // todos2: this.getData()
     }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(id) {
+    this.setState((prevState) => {
+      const updatedTodos = prevState.todos.map((el) => {
+        if (el.id === id)
+          el.completed = !el.completed
+        return el;
+      }
+      )
+      return {
+        todos: updatedTodos
+      }
+    })
+  }
+
+  getData() {
+    var mysql = require('mysql')
+    var connection = mysql.createConnection({
+      host: '95.165.129.236',
+      user: 'root',
+      password: 'ewe4',
+      database: 'gru'
+    });
+
+    connection.connect()
+
+    connection.query('SELECT * FROM users', function (err, rows, fields) {
+      if (err) throw err
+
+      console.log('The solution is: ', rows)
+    })
+
+    connection.end()
   }
 
   render() {
-    const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} />)
+    const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange} />)
 
     return (
       <div>
