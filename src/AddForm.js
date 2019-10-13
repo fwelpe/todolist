d1901351e017ae86b1468406a8aeec721992b2e5
fwelpe <arestovfa@gmail.com>
2019-10-14 00:13:25 +0300

    dunno why crash

--- src/AddForm.js
+++ src/AddForm.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const AddForm = () => {
+/* const AddForm = (props) => {
     const [todo, todoSet] = useState('')
     const newTodosObj = JSON.parse(localStorage.getItem('todolist')) || {}
 
@@ -34,6 +34,19 @@ const AddForm = () => {
             <input type="submit" value="Отправить" />
         </form>
     )
-}
+} */
 
-export default AddForm
+export default function (props) {
+    const [todo, todoSet] = useState('')
+    const handleChange = (event) => todoSet(event.target.value)
+
+    return (
+        <form onSubmit={props.handleSubmit({ todo: todo, completed: false })}>
+            <label>
+                Новая задача:
+            <input type="text" onChange={handleChange} />
+            </label>
+            <input type="submit" value="Отправить" />
+        </form>
+    )
+}
--- src/App.js
+++ src/App.js
@@ -1,81 +1,89 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Header from './Header.js';
 import TodoItem from './TodoItem'
 import AddForm from './AddForm'
 
 /* class App extends React.Component {
-  constructor() {
-    super()
-    this.state = {
-      todos: todosData,
-      // todos2: this.getData()
-    }
-    this.handleChange = this.handleChange.bind(this)
-  }
-
-  handleChange(id) {
-    this.setState((prevState) => {
-      const updatedTodos = prevState.todos.map((el) => {
-        if (el.id === id)
-          el.completed = !el.completed
-        return el;
-      }
-      )
-      return {
-        todos: updatedTodos
-      }
-    })
-  }
-
-  getData() {
-    const data = {
-      l: 'root',
-      p: 'ewe4',
-      req: 'insert into todolist (todo) values ("vsdf")'
+    constructor() {
+        super()
+        this.state = {
+            todos: todosData,
+            // todos2: this.getData()
+        }
+        this.handleChange = this.handleChange.bind(this)
     }
 
-    fetch('http://95.165.129.236:8080/r.php', {
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-      },
-      method: "POST",
-      body: JSON.stringify(data)
-    })
+    handleChange(id) {
+        this.setState((prevState) => {
+            const updatedTodos = prevState.todos.map((el) => {
+                if (el.id === id)
+                    el.completed = !el.completed
+                return el;
+            }
+            )
+            return {
+                todos: updatedTodos
+            }
+        })
+    }
 
-  }
+    getData() {
+        const data = {
+            l: 'root',
+            p: 'ewe4',
+            req: 'insert into todolist (todo) values ("vsdf")'
+        }
 
-  render() {
-    const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange} />)
+        fetch('http://95.165.129.236:8080/r.php', {
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            method: "POST",
+            body: JSON.stringify(data)
+        })
 
-    return (
-      <div>
-        <Header />
-        <div className="todo-list">
-          <AddForm />
-          {todoItems}
-        </div>
-        <Footer />
-      </div>
-    )
-  }
+    }
+
+    render() {
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange} />)
+
+        return (
+            <div>
+                <Header />
+                <div className="todo-list">
+                    <AddForm />
+                    {todoItems}
+                </div>
+                <Footer />
+            </div>
+        )
+    }
 } */
 
-const App = () => {
-  useEffect(() => {
-    const todosObj = JSON.parse(localStorage.getItem('todolist'));
-    const todoItems = [];
+function App() {
+  function getTodoArr() {
+    const todoItemsArr = [];
+    const todosObj = JSON.parse(localStorage.todolist);
     for (let keyIter in todosObj) {
-      todoItems.push(<TodoItem item={todosObj[keyIter]} />);
+      todoItemsArr.push(<TodoItem item={todosObj[keyIter]} />);
     }
-  }, [ localStorage.getItem('todolist') ])
+    return todoItemsArr;
+  }
+
+  const [todoItems, todoItemsUpd] = useState(getTodoArr());
+
+  function updateTodo(newTodoObj) {
+    todoItemsUpd([...todoItems, <TodoItem item={newTodoObj} />])
+
+  }
 
   return (
     <div>
       <Header />
       <div className="todo-list">
-        <AddForm />
+        <AddForm handleSubmit={updateTodo} />
         {todoItems}
       </div>
     </div>
