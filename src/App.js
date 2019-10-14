effbc9266f303f2da30fecbad645f4bd27f5af57
fwelpe <arestovfa@gmail.com>
2019-10-14 13:15:39 +0300

    working variant // no storageService (next step)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Header from './Header.js';
 import TodoItem from './TodoItem'
@@ -63,27 +63,40 @@ import AddForm from './AddForm'
 } */
 
 function App() {
-	const [todoItems, setTodoItems] = useState([]);
-	
-	useEffect(() => {
+	function getTodoArr() {
 		const todoItemsArr = [];
-		const todosObj = JSON.parse(localStorage.todolist);
-		for (let keyIter in todosObj) {
-			todoItemsArr.push(<TodoItem item={todosObj[keyIter]} />);
+		if (localStorage.todolist) {
+			const todosObj = JSON.parse(localStorage.todolist);
+			for (let keyIter in todosObj) {
+				todoItemsArr.push(<TodoItem item={todosObj[keyIter]} />);
+			}
 		}
-		setTodoItems(todoItemsArr);
-	}, [ todoItems ])
+		return todoItemsArr;
+	}
 
+	const [todoItems, setTodoItems] = useState(getTodoArr());
 
-	function updateTodo(newTodoObj) {
-		// setTodoItems([...todoItems, <TodoItem item={newTodoObj} />])
+	function newTodo(todoObj) {
+		const newTodosObj = JSON.parse(localStorage.getItem('todolist')) || {}
+		function getNewIndex() {
+			const ids = Object.keys(newTodosObj)
+			const getIndex = (acc, val) => {
+				const numVal = Number(val)
+				return numVal >= acc ? numVal + 1 : acc
+			}
+			return ids.reduce(getIndex, 0)
+		}
+		const newIndex = getNewIndex()
+		setTodoItems([...todoItems, <TodoItem key={newIndex} item={todoObj} />])
+		newTodosObj[newIndex] = todoObj
+		localStorage.setItem('todolist', JSON.stringify(newTodosObj))
 	}
 
 	return (
 		<div>
 			<Header />
 			<div className="todo-list">
-				<AddForm handleSubmit={updateTodo} />
+				<AddForm handleSubmit={newTodo} />
 				{todoItems}
 			</div>
 		</div>
