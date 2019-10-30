d3571f222f184bdb1562bb5ec7e05b542eb6aae1
fwelpe <arestovfa@gmail.com>
2019-10-30 18:59:09 +0300

    webStorrm

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
 
 const Header = () => {
+    const key = '3eb3f8b5e93b49c844e8317fe0e4e210';
+    const [ weatherRow, setWeatherRow ] = useState('');
+
+    useEffect(() => {
+        fetch(`http://api.openweathermap.org/data/2.5/weather?id=524901&APPID=${key}&units=metric`)
+            .then((r) => r.json())
+            .then((r) => {
+                const row = Object.keys(r.main).map((v) => {
+                    const result = v + ': ' + r.main[v];
+                    return result;
+                }).join(' ')
+                setWeatherRow(row);
+                console.log(r.main);
+            })
+    }, [])
+
     return (
-        <header className="navbar">TodoList</header>
+        <div className="header">
+            <header className="navbar"><div id={'weather'}>{weatherRow}</div><div id={'title'}>TodoList</div></header>
+        </div>
     )
 }
 
