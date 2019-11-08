e8a1e6cb3cb055649317fda3dde7edc4a628b99c
fwelpe <arestovfa@gmail.com>
2019-11-08 18:19:52 +0300

    Updates

---
+++ precache-manifest.e055b62cccbb9b82c48fdcbf092f782a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b46187aeb5af850222ed6d231a5467e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "81fd308dcfe9eff95f83",
+    "url": "/static/css/2.ef8e8b31.chunk.css"
+  },
+  {
+    "revision": "3a3a6cd9d9b76dbcd27f",
+    "url": "/static/css/main.835ad4f3.chunk.css"
+  },
+  {
+    "revision": "81fd308dcfe9eff95f83",
+    "url": "/static/js/2.a5391186.chunk.js"
+  },
+  {
+    "revision": "3a3a6cd9d9b76dbcd27f",
+    "url": "/static/js/main.953cfcc1.chunk.js"
+  },
+  {
+    "revision": "c83f1368d311e59c57b4",
+    "url": "/static/js/runtime-main.3ee827b8.js"
+  }
+]);
